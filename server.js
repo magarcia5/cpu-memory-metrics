9282ed54002ddd256960620477ce82b18f8e3866
Mel Alrajhi <malrajhi@linkedin.com>
2019-02-13 18:01:10 -0800

    displaying aggregate data successfully

--- server.js
+++ server.js
@@ -2,17 +2,42 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
+const { exec } = require('child_process');
+
+// number of lines in `top` output that contain aggregate data
+const NUM_AGGREGATE_LINES = 11;
 
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   console.log('a user connected');
+  const updateInterval = setInterval(updateUI, 3000);
+
+  socket.on('disconnect', () => {
+    clearInterval(updateInterval);
+  });
 });
 
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+function updateUI() {
+
+  exec('top -o cpu -l 1 -n 2', (err, stdout, stderr) => {
+    if (err) {
+      // in the event of an error, we'll just ignore it and try again on the next interval
+      // if the error persists, we could possibly close the connection
+      return;
+    }
+
+    const output = stdout.split('\n');
+    const aggregate = output.slice(0, NUM_AGGREGATE_LINES);
+  
+    io.emit('update', aggregate, '');
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 const io = require('socket.io-client');
 
-console.log("bowl again");
-const socket = io(); 
+const socket = io();
+
+// the index of the time value when splitting aggregate data into an array
+// not considered useful as it is not a metric
+const TIME_INDEX = 1;
+
+socket.on('update', function(aggregate, msg) {
+  document.getElementById('aggregate-info').innerHTML = displayAggregate(aggregate);
+});
+
+function displayAggregate(data) {
+  let html = '';
+
+  // remove time
+  data.splice(TIME_INDEX,1);
+
+  // remove all whitespace elements
+  const noWhiteSpaces = data.filter((d) => d.trim() !== '');
+
+  noWhiteSpaces.forEach(element => {
+    html = html.concat(`${element}<br>`);
+  });
+
+  return html;
+}
